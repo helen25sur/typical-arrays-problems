730d3b45465aa67c5e58a11fd924be39856de7f6
Elena Surilova <elen21surilova@gmail.com>
2021-03-09 23:50:03 +0200

    feat: add solution Tipical arrays problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+	if (array === undefined || array === null || array.length === 0) {
+		return 0;
+	}
+	let minValue = array[0];
+	for (let i = 0; i < array.length; i++) {
+		if (array[i] < minValue) {
+			minValue = array[i];
+		}
+	}
+	return minValue;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+	if (array === undefined || array === null || array.length === 0) {
+		return 0;
+	}
+	let maxValue = array[0];
+	for (let i = 0; i < array.length; i++) {
+		if (array[i] > maxValue) {
+			maxValue = array[i];
+		}
+	}
+	return maxValue;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+	if (array === undefined || array === null || array.length === 0) {
+		return 0;
+	}
+	let sum = 0;
+	const lengthArr = array.length;
+	for (let i = 0; i < lengthArr; i++) {
+		sum += array[i];
+	}
+	return sum / lengthArr;
 }
